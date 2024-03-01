b0c7bf9fb6e685de5283a5ea07f74da54cbffad7
Mohammed Afzaal <mohdafzaal71@gmail.com>
2024-03-02 01:35:18 +0300

    Added Tab Navigation

--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,17 +1,34 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
-import * as WebBrowser from "expo-web-browser";
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
+import { useOAuth } from '@clerk/clerk-expo';
 
 WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
   useWarmUpBrowser();
 
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error('OAuth error', err);
+    }
+  }, []);
 
   return (
     <View>
       <Image
-        source={require("./../../assets/images/login.jpg")}
+        source={require('./../../assets/images/login.jpg')}
         className="w-full h-[400px] object-cover"
       />
       <View className="p-8 bg-white mt-[-20px] rounded-t-3xl">
@@ -21,7 +38,7 @@ export default function LoginScreen() {
           money
         </Text>
         <TouchableOpacity
-          onPress={() => alert("Login")}
+          onPress={onPress}
           className="p-4 bg-blue-500 rounded-full mt-20"
         >
           <Text className="text-white text-center text-[18px]">
