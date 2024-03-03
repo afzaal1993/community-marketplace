14c506ffb7693517bf58c35cc1bd9ee77c6b9b2a
Moahmmed A. Faroq <mafzaal@taqnia.com>
2024-03-03 17:07:13 +0300

    03032024

---
+++ firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLBaxTUORYbRO0LJWK1frLRwklvy1ZOkc",
+  authDomain: "community-marketplace-59817.firebaseapp.com",
+  projectId: "community-marketplace-59817",
+  storageBucket: "community-marketplace-59817.appspot.com",
+  messagingSenderId: "260179245458",
+  appId: "1:260179245458:web:da045a25016556ee3ba94b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
